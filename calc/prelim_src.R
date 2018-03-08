6db36c947349faf640986a2c862c5eca02872665
pwkraft <kraft.pw@gmail.com>
2018-03-07 19:14:06 -0500

    work on preliminary results

--- calc/prelim_src.R
+++ calc/prelim_src.R
@@ -50,22 +50,19 @@ plot_df <- mft_pair %>%
 ggplot(plot_df, aes(x=key, y=mean, ymin=cilo, ymax=cihi)) +
   geom_point() + geom_errorbar(width=0) + geom_hline(yintercept = 0) + theme_bw()
 
-## select political topics
-sort(union(grep("politic", data_pair$op_text), grep("politic", data_pair$op_title)))
-
 checkTerm <- function(x, return_data = FALSE){
   cases <- c(map(x, grep, data_pair$op_text), map(x, grep, data_pair$op_title)) %>% 
-               unlist() %>% unique() %>% sort()
+    unlist() %>% unique() %>% sort()
   
   plot_df <- mft_pair[cases,] %>% 
     gather() %>%
-    mutate(key = factor(key, levels = rev(c("Authority", "Ingroup", "Purity", "Fairness", "Harm"
-                                            , "MFTcombined", "MoralityGeneral"))
-                        , labels = rev(c("Authority", "Ingroup", "Purity", "Fairness", "Harm"
+    mutate(key = factor(key, levels = rev(c("Harm", "Fairness", "Ingroup", "Authority"
+                                            , "Purity", "MFTcombined", "MoralityGeneral"))
+                        , labels = rev(c("Care", "Fairness", "Loyalty", "Authority", "Sanctity"
                                          , "Foundations\nCombined", "General\nMorality")))) %>%
     group_by(key) %>%
     summarise(mean = mean(value), sd = sd(value), n = n()) %>%
-    mutate(se = sd/sqrt(n), cilo = mean - se, cihi = mean + se)
+    mutate(se = sd/sqrt(n), cilo = mean - 1.96 * se, cihi = mean + 1.96 * se)
   
   if(return_data) return(plot_df)
   
@@ -75,13 +72,45 @@ checkTerm <- function(x, return_data = FALSE){
   ggplot(plot_df, aes(y=key, x=mean, xmin=cilo, xmax=cihi)) + 
     theme_classic() + theme(panel.border = element_rect(fill=NA)) +
     geom_point() + geom_errorbarh(height=0) + geom_vline(xintercept = 0) +
-    labs(title = paste0("Search Query (",length(cases)," hits):"), subtitle = paste(x, collapse="; ")
-         , x = NULL, y = "Moral Foundation") +
-    scale_x_continuous(limits=xlim, breaks=.9*xlim, labels = c("Less Persuasive", "More Persuasive")) +
+    labs(title = paste0("Search Query (",length(cases)," argument pairs):"), subtitle = paste(x, collapse="; ")
+         , x = "Difference in Percentage of Dictionary Terms (Delta - No Delta)", y = "Moral Foundation") +
+    #scale_x_continuous(limits=xlim, breaks=.9*xlim, labels = c("Less Persuasive", "More Persuasive")) +
+    scale_x_continuous(limits=xlim) +
     theme(axis.ticks.x=element_blank())
 }
 
-checkTerm("")
+mftTerms <- function(x){
+  map(x, grep, names(dict)) %>% 
+    unlist() %>% unique %>% sort() %>%
+    map(~gsub("*","", dict[[.]], fixed=T)) %>%
+    unlist()
+}
+
+
+checkTerm("") + labs(title = paste0("All Argument Pairs (n = ",nrow(mft_pair),")")
+                     , subtitle = NULL, y = "Moral Foundation"
+                     , x = "Difference in Percentage of Dictionary Terms (Delta - No Delta)")
+checkTerm(mftTerms("HarmVirtue"))
+checkTerm(mftTerms("HarmVice"))
+checkTerm(mftTerms("FairnessVirtue"))
+checkTerm(mftTerms("FairnessVice"))
+checkTerm(mftTerms("IngroupVirtue"))
+checkTerm(mftTerms("IngroupVice"))
+checkTerm(mftTerms("AuthorityVirtue"))
+checkTerm(mftTerms("AuthorityVice"))
+checkTerm(mftTerms("PurityVirtue"))
+checkTerm(mftTerms("PurityVice"))
+
+checkTerm(mftTerms("Harm"))
+checkTerm(mftTerms("Fairness"))
+checkTerm(mftTerms("Ingroup"))
+checkTerm(mftTerms("Authority"))
+checkTerm(mftTerms("Purity"))
+
+
+## select political topics
+sort(union(grep("politic", data_pair$op_text), grep("politic", data_pair$op_title)))
+
 checkTerm("politic")
 checkTerm("politic", T)
 checkTerm(c("liberal","conservati"))
@@ -92,23 +121,6 @@ checkTerm(c("climate change","global warming"))
 checkTerm(c("politic","liberal","conservati","democrat","republican","president"
             ,"climate change","global warming"))
 
-mftTerms <- function(x){
-  map(x, grep, names(dict)) %>% 
-    unlist() %>% unique %>% sort() %>%
-    map(~gsub("*","", dict[[.]], fixed=T)) %>%
-    unlist()
-}
-
-checkTerm(mftTerms("HarmVirtue"))
-checkTerm(mftTerms("HarmVice"))
-checkTerm(mftTerms(c("HarmVice","HarmVirtue")))
-checkTerm(mftTerms("Harm"))
-checkTerm(mftTerms("FairnessVirtue"))
-checkTerm(mftTerms("FairnessVice"))
-checkTerm(mftTerms("Purity"))
-
-checkTerm(mftTerms("Vice"))
-checkTerm(mftTerms("Virtue"))
 
 
 
@@ -129,7 +141,7 @@ plot_df <- mft_op %>%
                                               , "Fairness", "Harm", "General\nMorality")))) %>%
   group_by(foundation,Delta) %>%
   summarise(mean = mean(proportion), sd = sd(proportion), n = n()) %>%
-  mutate(se = sd/sqrt(n), cilo = mean - se, cihi = mean + se)
+  mutate(se = sd/sqrt(n), cilo = mean - 1.96 * se, cihi = mean + 1.96 * se)
 
 ggplot(plot_df, aes(y=foundation, x=mean, xmin=cilo, xmax=cihi, col=Delta, shape=Delta)) + 
   theme_classic() + theme(panel.border = element_rect(fill=NA)) + 
