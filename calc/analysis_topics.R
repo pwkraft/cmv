0e6c1d4fe9f2716be78c78e15030526e20cefd7e
pwkraft <kraft.pw@gmail.com>
2018-04-10 23:58:19 -0400

    add stm analysis

--- calc/analysis_topics.R
+++ calc/analysis_topics.R
@@ -4,6 +4,7 @@ library(purrr)
 library(ggplot2)
 library(quanteda)
 library(topicmodels)
+library(stm)
 
 ## load data
 load("out/cmv_data.Rdata")
@@ -48,3 +49,84 @@ tibble(terms = apply(terms(lda, 10),2,paste, collapse=", "),
   geom_point(size=1) + plot_default + xlim(0,.11) +
   labs(y="Topic Label", x="Average Topic Proportion")
 ggsave("fig/topics.pdf", width = 6.5, height = 3)
+
+
+
+##################################
+### STM analysis of topics in original post (Delta vs. No Delta)
+##################################
+
+## process for stm
+processed <- textProcessor(apply(data_op[,c("title","text")], 1, paste, collapse=" "),
+                           metadata = data.frame(delta = as.numeric(data_op$Delta)))
+out <- prepDocuments(processed$documents, processed$vocab, processed$meta,
+                     lower.thresh = 10)
+
+## stm fit with 20 topics
+stm_fit <- stm(out$documents, out$vocab, prevalence = as.matrix(out$meta)
+               , K=20, seed=12345)
+summary(stm_fit)
+
+## plot topic proportions
+pdf("fig/stm_op_prop.pdf", width=11, height=6)
+par(mar=c(4.2,0.5,0.5,0.5))
+plot(stm_fit, n=10, labeltype = "prob", text.cex = 1, main = NA)
+dev.off()
+
+## estimate topic prevalence effects
+prep <- estimateEffect(~ pos, stm_fit, meta = out$meta, uncertainty = "Global")
+
+## sort topics by effect size
+tmp <- tibble(estimate = sapply(summary(prep)[[3]], function(x) x["delta","Estimate"]),
+              topics = prep$topics) %>% arrange(estimate)
+
+## plot topic differences between negative and positive responses
+pdf("fig/stm_op_diff.pdf", height=6, width=11)
+par(mar=c(2.2,0.5,0.5,0.5))
+plot.estimateEffect(prep, covariate = "pos", model = stm_fit, topics = tmp$topics
+                    , method = "difference", cov.value1 = 1, cov.value2 = 0
+                    , labeltype = "prob", n=10, verbose.labels = F, width=100
+                    , xlim = c(-.05,.012)
+                    , xlab = "Difference in Topic Proportions (Change - No Change)")
+dev.off()
+
+
+
+##################################
+### STM analysis of topics in positive/negative responses
+##################################
+
+## process for stm
+processed <- textProcessor(c(data_pair$neg_text, data_pair$pos_text),
+                           metadata = data.frame(pos = rep(c(0,1), each=nrow(data_pair))))
+out <- prepDocuments(processed$documents, processed$vocab, processed$meta,
+                     lower.thresh = 10)
+
+## stm fit with 20 topics
+stm_fit <- stm(out$documents, out$vocab, prevalence = as.matrix(out$meta)
+                   , K=20, seed=12345)
+summary(stm_fit)
+
+## plot topic proportions
+pdf("fig/stm_pair_prop.pdf", width=11, height=6)
+par(mar=c(4.2,0.5,0.5,0.5))
+plot(stm_fit, n=10, labeltype = "prob", text.cex = 1, main = NA)
+dev.off()
+
+## estimate topic prevalence effects
+prep <- estimateEffect(~ pos, stm_fit, meta = out$meta, uncertainty = "Global")
+
+## sort topics by effect size
+tmp <- tibble(estimate = sapply(summary(prep)[[3]], function(x) x["pos","Estimate"]),
+              topics = prep$topics) %>% arrange(estimate)
+
+## plot topic differences between negative and positive responses
+pdf("fig/stm_pair_diff.pdf", height=6, width=11)
+par(mar=c(2.2,0.5,0.5,0.5))
+plot.estimateEffect(prep, covariate = "pos", model = stm_fit, topics = tmp$topics
+                    , method = "difference", cov.value1 = 1, cov.value2 = 0
+                    , labeltype = "prob", n=10, verbose.labels = F, width=100
+                    , xlim = c(-.05,.012)
+                    , xlab = "Difference in Topic Proportions (Change - No Change)")
+dev.off()
+
